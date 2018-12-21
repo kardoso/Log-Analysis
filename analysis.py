e691152fc60a8176c2737bc1a87513f1daf1cdc5
Samuel Cardoso <scksamuel.sc@gmail.com>
2018-12-21 13:09:30 -0300

    Estrutura do programa

---
+++ analysis.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Este programa responde as seguintes perguntas :
+#
+# 1. Quais são os três artigos mais populares de todos os tempos?
+# 2. Quem são os autores de artigos mais populares de todos os tempos?
+# 3. Em quais dias mais de 1% das requisições resultaram em erros?
+
+
+# executar uma query
+def get_query(query):
+    return 'results'
+
+
+# executa as queries e retorna o documento com as respostas
+def get_response():
+    return 'response'
+
+
+def main():
+    print(get_response)
+
+
+main()
